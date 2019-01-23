d46b4425eb853c75007b9bbfe6c84bd27ebfa235
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-23 12:37:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(currentLine, name) {
-  currentLine.unshift(name)
+  currentLine.push(name)
   return 'Welcome, ' + name + '. You are number '+ currentLine.length + ' in line.'
 }
 
