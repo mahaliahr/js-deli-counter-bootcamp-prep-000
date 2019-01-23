409d159ee14029a7046516dd4ad1d9d9dc3183d6
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-23 11:20:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(currentLine, name) {
-  return 'Welcome, ' + name + '. You are number '+ currentLine.length + ' in line.'
+  return 'Welcome, ' + name + '. You are number '+ (currentLine.length +1) + ' in line.'
 }
