75b170bb7b20024ddb77f9729771e239063ec086
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-23 13:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function nowServing(katzDeliLine) {
 function currentLine(currentLine) {
   var x = []
     for(var counter = 1; counter < currentLine.length; counter++ ) {
-      x.push('The line is currently: ' + counter + '. ' + currentLine[counter-1])
+      x.push(counter + '. ' + currentLine[counter-1])
     }
   if (currentLine.length === 0) {
     return 'The line is currently empty.'
 } else {
-  return x
+  return 'The line is currently:' + x
 }
 
 }
