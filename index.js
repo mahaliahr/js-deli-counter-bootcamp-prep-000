58265b8dd0c27d3dacc97895c0865f2fbd6503f2
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-23 13:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,13 +19,8 @@ function currentLine(currentLine) {
     }
   if (currentLine.length === 0) {
     return 'The line is currently empty.'
-
 } else {
-  return x
+  return 
 }
 
 }
-
-
-
-  return x
